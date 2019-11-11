3d7205c8135d1284354c8983dba630a39b2449cd
Sachin Gurung <saching061@hotmail.com>
2019-11-10 18:37:07 -0800

    Commit All

--- WhichNumberIsGreater/Program.cs
+++ WhichNumberIsGreater/Program.cs
@@ -10,6 +10,25 @@ namespace WhichNumberIsGreater
     {
         static void Main(string[] args)
         {
+            int firstNumber;
+            int secondNumber;
+            Console.Write("Enter first number: ");
+            firstNumber = int.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            secondNumber = int.Parse(Console.ReadLine());
+            if (firstNumber > secondNumber)
+                Console.WriteLine(firstNumber + " is greater than " + secondNumber);
+            else
+                Console.WriteLine(secondNumber + " is greater than " + firstNumber);
+
         }
     }
 }
+/*
+Sample Result:
+
+Enter first number: 65
+Enter second number: 45
+65 is greater than 45
+Press any key to continue . . .
+*/
